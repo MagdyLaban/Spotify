49d565f423b54f3889c58748d1b2307b4721f416
Magdy Laban <magdylaban3@gmail.com>
2020-09-15 23:30:45 +0200

    intial commit

---
+++ android/app/src/main/kotlin/com/app/spotifynew/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.spotifynew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF1DB954);
+const kPrimaryDarkColor = Color(0xFF191414);
