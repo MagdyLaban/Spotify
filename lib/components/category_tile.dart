93238aeaa9afd2826b84dd8a6515227974baf976
Hossam Smesm <hossam22.smesm@gmail.com>
2020-09-26 18:23:18 +0200

    making some refactor

--- lib/components/category_tile.dart
+++ lib/components/category_tile.dart
@@ -3,25 +3,31 @@ import 'package:flutter/material.dart';
 class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 10.0, top: 10.0),
-        child: Text(
-          'Category',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.w700
+    return InkWell(
+      onTap: (){
+        
+      },
+      child: Container(
+
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+          child: Text(
+            'Category',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700
+            ),
           ),
         ),
-      ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Color(0xff046d50), Colors.greenAccent]
-          ),
-        borderRadius: BorderRadius.circular(6.0)
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xff046d50), Colors.greenAccent]
+            ),
+          borderRadius: BorderRadius.circular(6.0)
+        ),
       ),
     );
   }
--- lib/screens/artists_screen.dart
+++ lib/screens/artists_screen.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:spotifynew/components/sliver_appbar_with_search.dart';
+import 'package:spotifynew/components/sliver_appbar.dart';
 import 'package:spotifynew/components/artists_tiles_sliver_grid.dart';
 import 'package:spotifynew/screens/bottom_nav_screens/bottom_nav_bar_screen.dart';
 import 'package:spotifynew/utilities/constant.dart';
@@ -24,6 +25,9 @@ class _ArtistsScreenState extends State<ArtistsScreen> {
             SliverAppBarWithSearch(
               text: 'Choose 3 or more artists you like.',
               style: kAppBarTitleTextStile,
+            ),
+            SliverPadding(
+              padding: EdgeInsets.all(5),
             ),
             ArtistsTilesSliverGrid(),
           ],
